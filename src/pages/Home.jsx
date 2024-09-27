9ae17d7ae71814ebcd7633f186e96a5d6fddf8f9
Krish_Bavarva <113332732+krishbavarva@users.noreply.github.com>
2024-09-27 13:36:34 +0530

    created catalog section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import CatalogSection from '../components/CatalogSection'
 
 export default function Home() {
   return (
     <>
         <HeroSection/>
+        <CatalogSection/>
     </>
+    
   )
 }
