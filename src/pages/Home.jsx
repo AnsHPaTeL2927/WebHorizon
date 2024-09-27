9074f638e14df7219a9e88a465f64cd8b41add7b
AnsHPaTeL2927 <116540143+AnsHPaTeL2927@users.noreply.github.com>
2024-09-27 13:51:59 +0530

    catalog section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import CatalogSection from '../components/CatalogSection'
 
 export default function Home() {
   return (
     <>
         <HeroSection/>
+        <CatalogSection/>
     </>
   )
 }
