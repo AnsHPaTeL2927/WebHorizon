3c56b6d4fb9a69cb2135a1fa80258c09f01397af
AnsHPaTeL2927 <116540143+AnsHPaTeL2927@users.noreply.github.com>
2024-09-27 23:32:27 +0530

    add updates in service cards

--- src/components/StepCard.jsx
+++ src/components/StepCard.jsx
@@ -11,14 +11,13 @@ const ServiceCard = ({ service }) => {
   const [expanded, setExpanded] = useState(false);
 
   const displayTasks = expanded ? service.tasks : service.tasks.slice(0, 5);
-
   const handleToggleExpand = () => {
     setExpanded(!expanded);
   };
 
   return (
-    <div className="flex flex-col items-center lg:flex-row items-start bg-black-800 border border-gray-700 rounded-lg shadow-lg hover:bg-white-700 transition duration-300 ease-in-out transform hover:scale-105 mx-auto max-w-full lg:max-w-4xl my-4">
-      
+    <div className="flex flex-col items-center lg:flex-row bg-black-800 border border-gray-700 rounded-lg shadow-lg hover:bg-white-700 transition duration-300 ease-in-out transform hover:scale-105 mx-auto max-w-full lg:max-w-4xl my-4">
+
       {/* Image Container - Responsive - Full Width for Mobile */}
       <div className="relative w-full lg:w-1/3">
         {/* Image with responsive sizing */}
@@ -28,35 +27,38 @@ const ServiceCard = ({ service }) => {
           alt="AC Service"
         />
         {/* Badge showing free offer */}
-        <div className="absolute top-2 left-0 bg-green-500 text-white text-xs font-bold px-2 py-1 rounded-r-lg">
+        <div className="absolute top-1 left-0 bg-green-500 text-white text-xs lg:text-sm font-bold px-2 py-1 rounded-r-lg">
           {service.free_offer}
         </div>
       </div>
 
       {/* Right Side Content */}
-      <div className="relative flex flex-col w-full lg:w-fit justify-between p-4 lg:p-6 leading-normal bg-black text-white rounded-lg lg:rounded-r-lg">
-        <div className="absolute top-1 right-1 bg-yellow-500 text-black text-xs font-bold px-2 py-1 rounded">{service.duration}</div>
+      <div className="relative flex flex-col w-full lg:w-fit justify-between p-4 sm:p-3 lg:p-6 leading-normal bg-black text-white rounded-lg lg:rounded-r-lg">
+        
+        {/* Duration badge */}
+        <div className="absolute top-1 right-1 bg-yellow-500 text-black text-xs lg:text-sm font-bold px-2 py-1 rounded">{service.duration}</div>
 
-        <p className="mb-2 text-lg font-bold text-gray-200">{service.description}</p>
+        {/* Description */}
+        <p className="mb-2 text-base sm:text-lg lg:text-xl font-bold text-gray-200">{service.description}</p>
 
         {/* Task List */}
         <div className="grid grid-cols-1 md:grid-cols-2 mb-2">
           {displayTasks.map((task, index) => (
-            <div key={index} className="flex items-center text-gray-200">
+            <div key={index} className="flex items-center text-sm sm:text-base lg:text-lg text-gray-200">
               {task.completed && checkmarkSVG}
               {task.name}
             </div>
           ))}
-          {/* View more/less option */}
           {!expanded && service.tasks.length > 5 && (
-            <div className="text-green-500 cursor-pointer underline" onClick={handleToggleExpand}>
+            <div className="text-green-500 cursor-pointer underline text-sm sm:text-base lg:text-lg" onClick={handleToggleExpand}>
               View All
             </div>
           )}
         </div>
 
+        {/* Show Less Option */}
         {expanded && service.tasks.length > 5 && (
-          <div className="text-green-500 cursor-pointer underline mb-2" onClick={handleToggleExpand}>
+          <div className="text-green-500 cursor-pointer underline mb-2 text-sm sm:text-base lg:text-lg" onClick={handleToggleExpand}>
             Show Less
           </div>
         )}
@@ -64,11 +66,11 @@ const ServiceCard = ({ service }) => {
         {/* Price and Add to Cart */}
         <div className="flex items-center justify-between mb-1">
           <div className="mb-2 sm:mb-0">
-            <span className="line-through text-gray-400">Rs. {service.previous_price}</span>
-            <span className="text-2xl font-bold ml-2 text-pink-500">₹ {service.current_price}</span>
+            <span className="line-through text-xs lg:text-sm text-gray-400">Rs. {service.previous_price}</span>
+            <span className="text-xl sm:text-2xl lg:text-3xl font-bold ml-2 text-pink-500">₹ {service.current_price}</span>
           </div>
           <div>
-            <button className="text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 inline-flex items-center dark:focus:ring-red-800">
+            <button className="text-xs sm:text-sm lg:text-base text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg px-4 py-2 inline-flex items-center dark:focus:ring-red-800">
               + ADD TO CART
             </button>
           </div>
